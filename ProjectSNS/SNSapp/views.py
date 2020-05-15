880bf9537c477ce2cd5266e9b6a0afa53169ee54
JIN <a1824jj@aiit.ac.jp>
2020-05-15 08:54:15 +0000

    create detail view

--- ProjectSNS/SNSapp/views.py
+++ ProjectSNS/SNSapp/views.py
@@ -42,3 +42,7 @@ def listfunc(request):
 def logoutfunc(request):
     logout(request)
     return redirect('login')
+
+def detailfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    return render(request, 'detail.html', {'object': object})
