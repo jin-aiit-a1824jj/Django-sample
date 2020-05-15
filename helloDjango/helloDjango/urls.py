f9457196cfd335b43730b60ba5c4fff5ca832109
JIN <a1824jj@aiit.ac.jp>
2020-05-15 02:01:50 +0000

    views.py

--- helloDjango/helloDjango/urls.py
+++ helloDjango/helloDjango/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import helloworldfunction
 
 urlpatterns = [
-    path('helloworld/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('helloworld/', helloworldfunction),
 ]
---
+++ helloDjango/helloDjango/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+def helloworldfunction(request):
+    returnobject = HttpResponse('<h1>hello world</h1>')
+    return returnobject
