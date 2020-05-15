a5008b2ae01102d84e465b71209ef775a3a41904
JIN <a1824jj@aiit.ac.jp>
2020-05-15 06:59:38 +0000

    check username duplication

--- ProjectSNS/SNSapp/views.py
+++ ProjectSNS/SNSapp/views.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def signupfunc(request):
+    user2 = User.objects.get(username='taro')
+    print(user2.email)
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        return render(request, 'signup.html', {'some': 100})
+        try:
+            User.objects.get(username=username)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some': 100})
+        
     return render(request, 'signup.html', {'some': 100})
