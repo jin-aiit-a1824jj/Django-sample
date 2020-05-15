48c11079b5b49d780bf0d240d64ae0a575eabfdf
JIN <a1824jj@aiit.ac.jp>
2020-05-15 07:08:05 +0000

    create login view

--- ProjectSNS/SNSapp/views.py
+++ ProjectSNS/SNSapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -17,3 +18,16 @@ def signupfunc(request):
             return render(request, 'signup.html', {'some': 100})
         
     return render(request, 'signup.html', {'some': 100})
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return redirect('login')
+
+    return  render(request, 'login.html')
