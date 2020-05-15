5808e5c9f36f68ae4ff41fa91c8c3b8adcf1d965
JIN <a1824jj@aiit.ac.jp>
2020-05-15 02:13:57 +0000

    add html

--- helloDjango/helloDjango/urls.py
+++ helloDjango/helloDjango/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import helloworldfunction
+from .views import helloworldfunction, HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('helloworld/', helloworldfunction),
+    path('helloworld2/', HelloWorldView.as_view())
 ]
