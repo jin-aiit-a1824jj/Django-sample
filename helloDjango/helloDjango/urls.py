fec77f030d80c8045b63ea2acb4bb5abe73d81e7
JIN <a1824jj@aiit.ac.jp>
2020-05-15 02:29:12 +0000

    implement view & set url in helloworldadpp

--- helloDjango/helloDjango/urls.py
+++ helloDjango/helloDjango/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import helloworldfunction, HelloWorldView
+from django.urls import path, include
+# from .views import helloworldfunction, HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('helloworld/', helloworldfunction),
-    path('helloworld2/', HelloWorldView.as_view())
+    #path('helloworld/', helloworldfunction),
+    #path('helloworld2/', HelloWorldView.as_view())
+    path('hello/', include('helloworldadpp.urls'))
 ]
